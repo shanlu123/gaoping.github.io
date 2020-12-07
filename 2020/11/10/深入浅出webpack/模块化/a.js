20e5b5858250252f4b24f8160cc35b2a0255892c
shanluzuode <3098387609@qq.com>
2020-12-07 14:50:25 +0800

    Site updated: 2020-12-07 14:50:21

---
+++ 2020/11/10/深入浅出webpack/模块化/a.js
@@ -0,0 +1,17 @@
+//1,CommonJS方式导出1
+// function hello(){
+//     console.log(hello,world)
+// }
+// module.exports={
+//     hello:hello
+// }
+
+//2,CommonJS方式导出2
+// exports.hello=function hello(a,b){
+//     console.log("hello world")
+// }
+
+
+//3,AMD方式
+
+//4，Es6模块化
---
+++ 2020/11/10/深入浅出webpack/模块化/b.js
@@ -0,0 +1,9 @@
+//1，CommonJS导入moudleA
+/**
+ * const moduleA=require('./a')
+ * moduleA.hello() // hello world
+ */
+
+ //2,AMD方式导入
+ 
+ //3，ES6模块化方式导入
